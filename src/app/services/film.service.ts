34a16458d54a55180743df08103491f4283ea424
Costa Gabriele Mattia <gabrielemattia.costa@reti.it>
2025-12-03 23:47:38 +0100

    Enhance movie display with carousel and genre-based recommendations; refactor film service API URL handling

--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -5,20 +5,21 @@ import { FilmResponse } from '../shared/film.interface';
 
 @Injectable({ providedIn: 'root' })
 export class FilmService {
-  page : number = 1;
-  genreId : number = 12;
+  page: number = 1;
+  genreId: number = 12;
   private apiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=it-IT&page=1&sort_by=popularity.desc&with_genres=12&with_original_language=it%7Cen`;
   private readonly apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDNhY2ZkMGU5M2IxZTg1ZTRlYTc0NGY4YjY1MGE3ZiIsIm5iZiI6MTc2MTkxMDUzNC45NjYsInN1YiI6IjY5MDQ5ZjA2YWQxNDNjM2RkMGRkNzkxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q3MK6-kSveHovhcZbBYNGk1oHZ9x65Fs7fyUoKubqB4';
 
   constructor(private http: HttpClient) {}
 
   buildApiUrl(genreId?: number, page?: number): string {
-    this.apiUrl.replace(/with_genres=\d+/, `with_genres=${genreId ?? this.genreId}`);
-    this.apiUrl.replace(/page=\d+/, `page=${page ?? this.page}`);
-    return this.apiUrl;
-}
+    let url = this.apiUrl;
+    url = url.replace(/with_genres=\d+/, `with_genres=${genreId ?? this.genreId}`);
+    url = url.replace(/page=\d+/, `page=${page ?? this.page}`);
+    return url;
+  }
 
-  getMoviesByGenre(genreId: number,page?: number): Observable<FilmResponse> {
+  getMoviesByGenre(genreId: number, page?: number): Observable<FilmResponse> {
     const url = this.buildApiUrl(genreId, page);
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.apiKey}`,
